3272862c761e34ee799ec0ae21109a6afedc993c
David <rac.david.salamon@gmail.com>
2022-04-20 20:13:17 +0200

    -

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 // creating express server
 const express = require('express');
-
 const path = require('path');
+const members = require('./members');
+const logger = require('./middleware/logger');
 
 const app = express();
 
+//app.use(logger);
+
 const PORT = process.env.PORT || 5000;
 
-// set static folder
+app.get('/api/members', (req, res) => {
+    res.json(members);
+});
+
+app.get('app/members/:id', (req, res) => {
+    res.json();
+});
+
+// set static folder, meaning it just get data into browser from file name in url (/about.html => about.html) 
 app.use(express.static(path.join(__dirname, 'public')));
 
 /*
---
+++ members.js
@@ -0,0 +1,23 @@
+
+const members = [
+    {
+        id: 1,
+        name: 'testing test',
+        email: 'test@david.com',
+        status: 'active'
+    },
+    {
+        id: 1,
+        name: 'niko nikic',
+        email: 'testing@testing.com',
+        status: 'inactive'
+    },
+    {
+        id: 1,
+        name: 'david salamon',
+        email: 'david@david.com',
+        status: 'active'
+    }
+];
+
+module.exports = members; 
